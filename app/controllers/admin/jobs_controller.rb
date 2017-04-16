05164bb28bd421fda38ab12ec206990d4ee41a06
chixinfeng <chendongchun520@gmail.com>
2017-04-17 05:52:19 +0800

    implement step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  layout "admin"
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = 'You are not admin'
@@ -9,6 +10,10 @@ class Admin::JobsController < ApplicationController
   end
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
 
   def index
@@ -49,6 +54,19 @@ class Admin::JobsController < ApplicationController
 
     redirect_to admin_jobs_path
   end
+
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
+  end
+
 
   private
 
